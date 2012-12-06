abccfa52722971d247d24ee57d463a0a04565c3e
Paul Komkoff <i@stingr.net>
2012-12-06 00:35:23 +0000

    Drop unused exception prototypes.

--- src/main/scala/Invoker.scala
+++
@@ -1,7 +0,0 @@
-package org.stingray.contester
-
-import org.stingray.contester.proto.Contester._
-
-class CompilationFailure(val result: Compilation) extends Throwable(result.toString)
-class CompilationGeneralFailure(reason: String) extends Throwable(reason)
-
