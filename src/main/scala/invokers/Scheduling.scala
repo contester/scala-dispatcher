f70e3d4c81cb0d7ce83d418a28f05ee49de21161
Paul Komkoff <i@stingr.net>
2013-06-03 20:04:28 +0100

    Comments.

--- src/main/scala/invokers/Scheduling.scala
+++ src/main/scala/invokers/Scheduling.scala
@@ -2,16 +2,31 @@ package org.stingray.contester.invokers
 
 import java.sql.Timestamp
 
-object SchedulingKey {
+private object TimeKey {
   val EARLIEST = new Timestamp(0)
   val LATEST = new Timestamp(Long.MaxValue)
 }
 
+/** If type is used for scheduling inside invoker registry, it needs to descend from this trait.
+  *
+  */
 trait SchedulingKey extends Ordered[SchedulingKey]
 
+/** Simplest and the only used now implementation of SchedulingKey.
+  *
+  */
 trait TimeKey extends SchedulingKey {
+  /** Override this to return timestamp of the key.
+    *
+    * @return Timestamp of the key.
+    */
   def timestamp: Timestamp
 
+  /** Compares this against other scheduling key. If it's a timestamp, compare timestamps.
+    *
+    * @param that Other scheduling key.
+    * @return What compare usually returns.
+    */
   def compare(that: SchedulingKey): Int =
     that match {
       case x: TimeKey =>
@@ -19,6 +34,9 @@ trait TimeKey extends SchedulingKey {
     }
 }
 
+/** TimeKey with timestamp set to the earliest possible value.
+  *
+  */
 trait EarliestTimeKey extends TimeKey {
-  val timestamp = SchedulingKey.EARLIEST
+  val timestamp = TimeKey.EARLIEST
 }
