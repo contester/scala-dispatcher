ed0153c34e21e5f2321191cac7decec0fe5a19b5
Paul Komkoff <i@stingr.net>
2013-06-03 19:56:31 +0100

    Protobuf tools

--- src/main/scala/utils/ProtobufTools.scala
+++ src/main/scala/utils/ProtobufTools.scala
@@ -3,24 +3,42 @@ package org.stingray.contester.utils
 import com.google.protobuf.Message
 import java.io.InputStream
 
+/** Parametrized functions to create protobufs.
+  * You will usually do createProtobuf[MessageType](value).
+  *
+  */
 object ProtobufTools {
   private[this] val ARRAY_OF_BYTE_ARRAY = Array[Class[_]](classOf[Array[Byte]])
   private[this] val ARRAY_OF_INPUT_STREAM = Array[Class[_]](classOf[InputStream])
 
-  def createProtobufFromBytes[I <: Message](bytes: Array[Byte])(implicit manifest: Manifest[I]): I = {
+  private def createProtobuf[I <: Message](bytes: Array[Byte])(implicit manifest: Manifest[I]): I = {
     manifest.runtimeClass.getDeclaredMethod("parseFrom", ARRAY_OF_BYTE_ARRAY: _*).invoke(null, bytes).asInstanceOf[I]
   }
 
-  def createProtobufFromInputStream[I <: Message](input: InputStream)(implicit manifest: Manifest[I]): I = {
+  /** Create protobuf from InputStream.
+    *
+    * @param input Stream to parse.
+    * @param manifest Manifest for I
+    * @tparam I Protobuf message.
+    * @return Parsed proto.
+    */
+  def createProtobuf[I <: Message](input: InputStream)(implicit manifest: Manifest[I]): I = {
     manifest.runtimeClass.getDeclaredMethod("parseFrom", ARRAY_OF_INPUT_STREAM: _*).invoke(null, input).asInstanceOf[I]
   }
 
-  def createEmptyProtobuf[I <: Message](implicit manifest: Manifest[I]): I = {
+  private def createEmpty[I <: Message](implicit manifest: Manifest[I]): I = {
     manifest.runtimeClass.getDeclaredMethod("getDefaultInstance").invoke(null).asInstanceOf[I]
   }
 
+  /** Create protobuf from Option[Array[Byte]].
+    * Will return default proto if argument was None.
+    *
+    * @param opt Array[Byte] with proto, or not.
+    * @param manifest
+    * @tparam I
+    * @return
+    */
   def createProtobuf[I <: Message](opt: Option[Array[Byte]])(implicit manifest: Manifest[I]): I =
-    opt.map(createProtobufFromBytes[I](_)).getOrElse(createEmptyProtobuf[I])
-
+    opt.map(createProtobuf[I](_)).getOrElse(createEmpty[I])
 }
 
