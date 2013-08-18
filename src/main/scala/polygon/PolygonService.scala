dd5811b10aff4f68a9d1fd73260cfe5b6709cf0c
Paul Komkoff <i@stingr.net>
2013-08-18 21:49:18 +0100

    Prototype of new polygoncache

--- src/main/scala/polygon/PolygonService.scala
+++ src/main/scala/polygon/PolygonService.scala
@@ -1,91 +1,67 @@
 package org.stingray.contester.polygon
 
-import com.twitter.util.{Time, Future}
+import com.twitter.util.{Await, Future}
 import org.stingray.contester.invokers.InvokerRegistry
 import org.stingray.contester.problems
 import problems._
 import org.stingray.contester.utils._
 import java.net.URL
-import com.google.common.collect.MapMaker
-import com.google.common.cache.CacheBuilder
+import com.google.common.cache.{CacheLoader, CacheBuilder}
 import scala.xml.XML
 import org.jboss.netty.buffer.ChannelBuffer
 import com.twitter.finagle.Service
-import scala.collection.mutable
+import java.util.concurrent.TimeUnit
+import com.google.common.util.concurrent.{SettableFuture, ListenableFuture}
 
 
 /// watch returns Future[Iterable[ContestDescription]]
 
+class ContestRepository(client: Service[PolygonClientRequest, ChannelBuffer], cache: Cache[ContestHandle, String]) {
+  object NearCacheReloader extends CacheLoader[ContestHandle, Future[ContestDescription]] {
+    def load(key: ContestHandle): Future[ContestDescription] =
+      nearFetch(key)
 
-class ContestSimpleWatcher(client: Service[PolygonClientRequest, ChannelBuffer], cache: Cache[ContestHandle, (String, Time)]) {
-  /**
-   * Maps the key to future for rescan action.
-   * Futures are replaced (not chained) for each rescan.
-   * To stop watching, remove a future & (optionally) cancel it.
-   */
-  private val watched = {
-    import scala.collection.JavaConverters._
-    new MapMaker().makeMap[ContestHandle, Future[Unit]].asScala
-  }
-
-  private val watchers = new mutable.HashMap[WatcherReceiver[ContestHandle, ContestDescription], Iterable[ContestHandle]]
-
-  private val localValueCache = CacheBuilder.newBuilder().build[ContestHandle, (ContestDescription, Time)]
-
-  private def transform(x: (String, Time)) =
-    (new ContestDescription(XML.loadString(x._1)), x._2)
-
-  private def storeLocal(key: ContestHandle, rawValue: (String, Time)): (ContestDescription, Time) = {
-    val result = transform(rawValue)
-    localValueCache.put(key, result)
-    result
-  }
-
-  // farfetch needs to wait if there's an exception/item not found.
-
-  private def farFetch(key: ContestHandle): Future[(String, Time)] =
-    client(key).map(PolygonClient.asPage).map(x => (x, Time.now))
-
-  // TODO: change notifications
-  private def fetchAndReplace(key: ContestHandle) =
-    farFetch(key).flatMap { v =>
-      cache.put(key, v).map(_ => storeLocal(key, v)._1)
-    }.ensure(rescheduleWatch(key))
-
-  private def rescheduleWatch(key: ContestHandle): Unit = {
-    import com.twitter.util.TimeConversions._
-    watched.replace(key, Utils.later(1.minute).flatMap(_ => fetchAndReplace(key).map(_ => rescheduleWatch(key))))
-  }
-
-  private def cachedFetch(key: ContestHandle): Future[ContestDescription] =
-    cache.get(key).flatMap(_.map(v => Future.value(storeLocal(key, v)._1))
-      .getOrElse(farFetch(key).flatMap { v =>
-        cache.put(key, v).map(_ => storeLocal(key, v)._1)
-    }))
-
-  def get(key: ContestHandle): Future[ContestDescription] =
-    Option(localValueCache.getIfPresent(key))
-      .map(v => Future.value(v._1))
-      .getOrElse(cachedFetch(key))
-
-  /**
-   * Set watched keys for the receiver.
-   * @param receiver
-   * @param keys
-   */
-  def watch(receiver: WatcherReceiver[ContestHandle, ContestDescription], keys: Iterable[ContestHandle]): Unit = {
-    synchronized {
-      val prev = watchers.values.flatten.toSet
-      watchers.put(receiver, keys)
-      val now = watchers.values.flatten.toSet
-
-      (prev -- now).foreach(watched.remove(_).foreach(_.cancel()))
-      (now -- prev).foreach(watched.putIfAbsent(_, Future.never))
+    override def reload(key: ContestHandle, oldValue: Future[ContestDescription]): ListenableFuture[Future[ContestDescription]] = {
+      val result = new SettableFuture[Future[ContestDescription]]
+      if (oldValue.isDefined) {
+        farFetch(key).map(transform)
+          .onSuccess {
+            v =>
+              result.set(if (v == Await.result(oldValue)) oldValue else Future.value(v))
+          }
+          .onFailure(e => result.setException(e))
+      } else {
+        result.set(oldValue)
+      }
+      result
     }
-    Future.collect(keys.map(k => get(k).map(k -> _)).toSeq)
   }
-}
 
+  private def farFetchReal(key: ContestHandle): Future[String] =
+    client(key).map(PolygonClient.asPage)
+
+  private def farFetch(key: ContestHandle) =
+    farSerial(key, () => farFetchReal(key))
+
+  private def nearFetch(key: ContestHandle) =
+    cache.get(key).flatMap { v =>
+      v.map(x => Future.value(transform(x)))
+       .getOrElse(farFetch(key).map(transform))
+    }
+
+  private def transform(x: String) =
+    (new ContestDescription(XML.loadString(x)))
+
+  private val nearFutureCache = CacheBuilder.newBuilder()
+    .expireAfterAccess(300, TimeUnit.SECONDS)
+    .refreshAfterWrite(60, TimeUnit.SECONDS)
+    .build(NearCacheReloader)
+
+  private val farSerial = new SerialHash[ContestHandle, String]
+
+  def apply(key: ContestHandle) =
+    nearFutureCache.get(key)
+}
 
 class ProblemByPid(client: SpecializedClient, pdb: PolygonDb) extends ScannerCache[ProblemURL, PolygonProblem, PolygonProblem] {
   def nearGet(key: ProblemURL): Future[Option[PolygonProblem]] =
