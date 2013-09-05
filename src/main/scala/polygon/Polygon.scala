106120ca429883cdb9499234f689ab586c28e743
Paul Komkoff <i@stingr.net>
2013-09-05 22:30:49 +0100

    Fix mingw.

--- src/main/scala/polygon/Polygon.scala
+++ src/main/scala/polygon/Polygon.scala
@@ -216,7 +216,7 @@ private object PolygonProblemUtils {
 
 class PolygonProblem(val source: Elem, val externalUrl: Option[URL]) extends ProblemDescription {
   override def toString = "PolygonProblem(%s, %d)".format(url, revision)
-  val pid = PolygonProblemUtils.getPdbPath(url)
+  def pid = PolygonProblemUtils.getPdbPath(url)
   val handle = new PolygonProblemHandle(url, Some(revision))
 
   override def equals(obj: Any): Boolean =
