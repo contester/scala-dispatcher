4d6e58c9f065b28fbd43ac525444cefc3c0fe619
Paul Komkoff <i@stingr.net>
2013-09-06 13:56:22 +0100

    Scaladocs and cleanups.

--- src/main/scala/problems/Problem.scala
+++ src/main/scala/problems/Problem.scala
@@ -4,12 +4,30 @@ import org.stingray.contester.invokers.Sandbox
 import com.twitter.util.Future
 import collection.immutable
 
-class ProblemProxy(parent: Problem, protected val tests: Seq[Int]) extends Problem {
+/**
+ * A proxy for parent problem to limit the test set.
+ * @param parent Problem.
+ * @param tests Test set limit.
+ */
+private class ProblemProxy(parent: Problem, protected val tests: Seq[Int]) extends Problem {
   def getTest(key: Int): Test = parent.getTest(key)
 }
 
+/**
+ * A problem from the testing engine point of view is a map TestID->Test.
+ */
 trait Problem extends immutable.SortedMap[Int, Test] {
+  /**
+   * Override this method to provide sequence of tests.
+   * @return Sequence of tests.
+   */
   protected def tests: Seq[Int]
+
+  /**
+   * Override this method to provide tests themselves.
+   * @param key Test ID.
+   * @return Test.
+   */
   def getTest(key: Int): Test
 
   implicit def ordering: Ordering[Int] = Ordering.Int
@@ -34,10 +52,26 @@ trait Problem extends immutable.SortedMap[Int, Test] {
       None
 }
 
+/**
+ * Identifier for a problem already in gridfs.
+ * Has helper methods for all gridfs file names.
+ */
 trait ProblemID {
+  /**
+   * Defines problem ID in pdb/gridfs. Needs to be storage-compatible.
+   * @return Problem ID in pdb/gridfs.
+   */
   def pid: String
+
+  /**
+   * Revision of a problem.
+   * @return Revision.
+   */
   def revision: Int
 
+  /**
+   * Constructed ID for a problem. Used as part of paths and as _id in manifest collection.
+   */
   final val pdbId = pid + "/" + revision.toString
 
   override def toString = "ProblemID(%s, %d)".format(pid, revision)
@@ -50,24 +84,94 @@ trait ProblemID {
     case _ => super.equals(obj)
   }
 
+  /**
+   * Basename used for sanitizing.
+   *
+   * TODO: Replace with shortened/hash?
+   * @return Filesystem-compatible name.
+   */
   def destName = pid.replace('/', '.').replace(':', '.') + "." + revision.toString
+
+  /**
+   * Archive name written out for saniziting.
+   * @return FIlesystem-compatible name.
+   */
   def zipName = destName + ".zip"
+
+  /**
+   * Prefix in gridfs for all problem-related things.
+   * @return Gridf-compatible prefix.
+   */
   def prefix = "problem/" +  pdbId
+
+  /**
+   * Shorthand for creating all other paths.
+   * @param suffix
+   * @return
+   */
   def dbName(suffix: String) =
     prefix + "/" + suffix
 
-  def manifestName = dbName("manifest")
-  def problemXmlName = dbName("problem.xml")
+  /**
+   * Checker path.
+   * @return
+   */
   def checkerName = dbName("checker")
+
+  /**
+   * Prefix for a given test id.
+   * @param testId Test id.
+   * @return
+   */
   def testPrefix(testId: Int) = dbName("tests/" + testId + "/")
+
+  /**
+   * Input data path.
+   * @param testId Test id.
+   * @return
+   */
   def inputName(testId: Int) = testPrefix(testId) + "input.txt"
+
+  /**
+   * Answer file path.
+   * @param testId Test id.
+   * @return
+   */
   def answerName(testId: Int) = testPrefix(testId) + "answer.txt"
+
+  /**
+   * Archive path.
+   * @return
+   */
   def archiveName = dbName("archive")
+
+  /**
+   * Interactor path.
+   * @return
+   */
   def interactorName = dbName("interactor")
 }
 
+/**
+ * Simplest problem ID, just by pid/revision.
+ * @param pid Problem pid.
+ * @param revision Problem revision.
+ */
 case class SimpleProblemID(override val pid: String, override val revision: Int) extends ProblemID
 
+/**
+ * A problem from testing engine point of view, built from ProblemID and additional info
+ * (pulled from manifest, presumably).
+ * @param pdb ProblemDB client.
+ * @param id ProblemID instance.
+ * @param testCount How many tests.
+ * @param timeLimitMicros Time limit in microseconds.
+ * @param memoryLimit Memory limit in bytes.
+ * @param testerName Original file name for checker.
+ * @param answers Set of test numbers for which answers are provided.
+ * @param interactorName Some(original file name for interactor) or none.
+ * @param stdio Is this a stdio problem or not.
+ */
 class PDBProblem(val pdb: ProblemDb, val id: ProblemID, val testCount: Int, val timeLimitMicros: Long,
                  val memoryLimit: Long, val testerName: String, val answers: Set[Int],
                  val interactorName: Option[String], val stdio: Boolean) extends Problem {
