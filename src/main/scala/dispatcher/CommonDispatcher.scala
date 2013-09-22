fe5342e1a00134ae86f1d1ae017c2c7bb12c4a0a
Paul Komkoff <i@stingr.net>
2013-09-22 21:58:14 +0100

    Add todo for common dispatcher class.

---
+++ src/main/scala/dispatcher/CommonDispatcher.scala
@@ -0,0 +1,12 @@
+package org.stingray.contester.dispatcher
+
+/*
+trait CommonDispatcherId {
+  def namespaceId: String
+  def itemId: I
+}
+
+abstract class CommonDispatcher[SubmitType <: HasId] {
+  val activeItems
+}
+*/
