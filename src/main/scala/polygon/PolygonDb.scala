4e6e6aff857d93da4d848750b19f5f4be1cb7b98
Paul Komkoff <i@stingr.net>
2016-03-30 16:11:45 +0100

    whee

--- src/main/scala/polygon/PolygonDb.scala
+++ src/main/scala/polygon/PolygonDb.scala
@@ -4,15 +4,33 @@ import com.twitter.util.Future
 import com.mongodb.casbah.Imports._
 import org.stingray.contester.utils.ValueCache
 import java.net.URL
+
+import com.twitter.finagle.redis.Client
+import com.twitter.finagle.redis.util.StringToChannelBuffer
+import com.twitter.io.Charsets
 import grizzled.slf4j.Logging
 
 trait PolygonCacheKey {
   def url: URL
 }
 
-trait PolygonContestKey extends PolygonCacheKey
+trait PolygonContestKey extends PolygonCacheKey with ProvidesRedisKey {
+  def redisKey = s"rawContest/${url.toString}"
+}
 trait PolygonProblemKey extends PolygonCacheKey {
   def revision: Option[Int]
+}
+
+trait ProvidesRedisKey {
+  def redisKey: String
+}
+
+class RedisStore(client: Client) extends ValueCache[ProvidesRedisKey, String] {
+  override def get(key: ProvidesRedisKey): Future[Option[String]] =
+    client.get(StringToChannelBuffer(key.redisKey)).map(_.map(_.toString(Charsets.Utf8)))
+
+  override def put(key: ProvidesRedisKey, value: String): Future[Unit] =
+    client.set(StringToChannelBuffer(key.redisKey), StringToChannelBuffer(value))
 }
 
 class PolygonCache(mdb: MongoDB) extends ValueCache[PolygonCacheKey, String] with Logging {
