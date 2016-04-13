06a09311b235636af647e80cf62879325903a8cc
Paul Komkoff <i@stingr.net>
2016-04-13 17:51:46 +0100

    Wheee!!!

--- src/test/scala/StrategyTests.scala
+++ src/test/scala/StrategyTests.scala
@@ -1,8 +1,7 @@
 package org.stingray.contester.testing
 
-import org.scalatest.FlatSpec
-import org.scalatest.matchers.ShouldMatchers
 import com.twitter.util.{Await, Future}
+import org.scalatest.{FlatSpec, Matchers}
 
 object ZeroTest extends TestingStrategy {
   def test(test: Solution.NumberedTest): Future[Solution.EvaluatedTestResult] =
@@ -14,36 +13,28 @@ class FailAt(x: Int) extends TestingStrategy {
     Future.value((x != test._1) -> (test._1, null))
 }
 
-class StrategyTest extends FlatSpec with ShouldMatchers {
+class StrategyTest extends FlatSpec with Matchers {
+  private val testCount = 100
+  private val sortedTests = (1 to testCount).map(_ -> null)
+  private val sortedResults = (1 to testCount).map(x => true -> (x -> null))
+
   "Sequential" should "be in ascending order" in {
-    expectResult((1 to 100).map(x => true -> (x -> null))) {
-      Await.result(ZeroTest.sequential((1 to 100).map(_ -> null)))
-    }
+    Await.result(ZeroTest.sequential(sortedTests)) shouldBe sortedResults
   }
   "Parallel" should "be in any order" in {
-    expectResult((1 to 100).map(x => true -> (x -> null))) {
-      Await.result(ZeroTest.parallel((1 to 100).map(_ -> null))).sortBy(_._2._1)
-    }
+    Await.result(ZeroTest.parallel(sortedTests)).sortBy(_._2._1) shouldBe sortedResults
   }
   "Sequential" should "stop at X" in {
-    expectResult((1 to 55).map(x => (x != 55) -> (x -> null))) {
-      Await.result(new FailAt(55).sequential((1 to 100).map(_ -> null)))
-    }
+    Await.result(new FailAt(55).sequential(sortedTests)) shouldBe (1 to 55).map(x => (x != 55) -> (x -> null))
   }
   "Parallel" should "not stop at X" in {
-    expectResult((1 to 100).map(x => (x != 55) -> (x -> null))) {
-      Await.result(new FailAt(55).parallel((1 to 100).map(_ -> null))).sortBy(_._2._1)
-    }
+    Await.result(new FailAt(55).parallel(sortedTests)).sortBy(_._2._1) shouldBe (1 to testCount).map(x => (x != 55) -> (x -> null))
   }
   "School" should "stop at 1" in {
-    expectResult((1 to 1).map(x => (x != 1) -> (x -> null))) {
-      Await.result(new FailAt(1).school((1 to 100).map(_ -> null)))
-    }
+    Await.result(new FailAt(1).school((1 to 100).map(_ -> null))) shouldBe (1 to 1).map(x => (x != 1) -> (x -> null))
   }
   "School" should "not stop at 2" in {
-    expectResult((1 to 100).map(x => (x != 2) -> (x -> null))) {
-      Await.result(new FailAt(2).school((1 to 100).map(_ -> null)))
-    }
+    Await.result(new FailAt(2).school((1 to 100).map(_ -> null))) shouldBe (1 to 100).map(x => (x != 2) -> (x -> null))
   }
 }
 
