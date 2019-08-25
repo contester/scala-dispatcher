1b122a6394baf147bbdff216ea42f5085443ec08
Paul Komkoff <i@stingr.net>
2019-08-25 19:55:07 +0100

    Replace reflection-based proto parsing with implicit lookups.

--- src/main/scala/common/Cache.scala
+++ src/main/scala/common/Cache.scala
@@ -1,19 +1,19 @@
 package org.stingray.contester.common
 
-import com.google.protobuf.Message
 import com.twitter.finagle.Memcached
 import com.twitter.io.Buf
 import com.twitter.util.{Future, Time}
 import grizzled.slf4j.Logging
 import org.stingray.contester.utils.ProtobufTools
+import scalapb.{GeneratedMessage, GeneratedMessageCompanion, Message}
 
 trait ObjectCache {
   def cacheGet(key: String): Future[Option[Buf]]
   def cacheSet(key: String, value: Buf, expiry: Option[Time]): Future[Unit]
 
-  def maybeCached[S, I <: Message](key: String, expiry: Option[Time],
+  def maybeCached[S, I <: GeneratedMessage with Message[I]](key: String, expiry: Option[Time],
                                            fetch: => Future[S], wrap: (I) => S,
-                                           unwrap: (S) => I)(implicit manifest: Manifest[I]): Future[S] =
+                                           unwrap: (S) => I)(implicit cmp: GeneratedMessageCompanion[I]): Future[S] =
     cacheGet(key).flatMap { optValue =>
       optValue
           .map(ProtobufTools.createProtobuf[I])
--- src/main/scala/utils/ProtobufTools.scala
+++ src/main/scala/utils/ProtobufTools.scala
@@ -1,59 +1,28 @@
 package org.stingray.contester.utils
 
-import com.google.protobuf.Message
-import java.io.InputStream
-
 import com.twitter.io.Buf
-import org.jboss.netty.buffer.ChannelBuffer
+import scalapb.{GeneratedMessage, GeneratedMessageCompanion, Message}
 
 /** Parametrized functions to create protobufs.
   * You will usually do createProtobuf[MessageType](value).
   *
   */
 object ProtobufTools {
-  private[this] val ARRAY_OF_BYTE_ARRAY = Array[Class[_]](classOf[Array[Byte]])
-  private[this] val ARRAY_OF_INPUT_STREAM = Array[Class[_]](classOf[InputStream])
-
-  def asByteArray(buffer: ChannelBuffer) = {
-    val bufferBytes = new Array[Byte](buffer.readableBytes())
-    buffer.getBytes(buffer.readerIndex(), bufferBytes)
-    bufferBytes
-  }
-
-  def createProtobuf[I <: Message](buffer: ChannelBuffer)(implicit manifest: Manifest[I]): I =
-    createProtobuf(asByteArray(buffer))
-
-
-  def createProtobuf[I <: Message](buffer: Buf)(implicit manifest: Manifest[I]): I =
+  def createProtobuf[I <: GeneratedMessage with Message[I]](buffer: Buf)(implicit cmp: GeneratedMessageCompanion[I]): I = {
     createProtobuf(Buf.ByteArray.Shared.extract(buffer))
-
-  def createProtobuf[I <: Message](bytes: Array[Byte])(implicit manifest: Manifest[I]): I = {
-    manifest.runtimeClass.getDeclaredMethod("parseFrom", ARRAY_OF_BYTE_ARRAY: _*).invoke(null, bytes).asInstanceOf[I]
   }
 
-  /** Create protobuf from InputStream.
-    *
-    * @param input Stream to parse.
-    * @param manifest Manifest for I
-    * @tparam I Protobuf message.
-    * @return Parsed proto.
-    */
-  def createProtobuf[I <: Message](input: InputStream)(implicit manifest: Manifest[I]): I = {
-    manifest.runtimeClass.getDeclaredMethod("parseFrom", ARRAY_OF_INPUT_STREAM: _*).invoke(null, input).asInstanceOf[I]
-  }
-
-  private def createEmpty[I <: Message](implicit manifest: Manifest[I]): I = {
-    manifest.runtimeClass.getDeclaredMethod("getDefaultInstance").invoke(null).asInstanceOf[I]
+  def createProtobuf[I <: GeneratedMessage with Message[I]](bytes: Array[Byte])(implicit cmp: GeneratedMessageCompanion[I]): I = {
+    cmp.parseFrom(bytes)
   }
 
   /** Create protobuf from Option[Array[Byte]].
     * Will return default proto if argument was None.
     *
     * @param opt Array[Byte] with proto, or not.
-    * @param manifest
     * @tparam I
     * @return
     */
-  def createProtobuf[I <: Message](opt: Option[Array[Byte]])(implicit manifest: Manifest[I]): I =
-    opt.map(createProtobuf[I](_)).getOrElse(createEmpty[I])
+  def createProtobuf[I <: GeneratedMessage with Message[I]](opt: Option[Array[Byte]])(implicit cmp: GeneratedMessageCompanion[I]): I =
+    opt.map(createProtobuf[I](_)).getOrElse(cmp.defaultInstance)
 }
