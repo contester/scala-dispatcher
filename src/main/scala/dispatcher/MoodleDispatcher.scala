754df22f982bf0bef3d0120289aa99aa664e37b1
Paul Komkoff <i@stingr.net>
2020-03-22 13:01:24 +0000

    Add uts fields in testing table

--- src/main/scala/dispatcher/MoodleDispatcher.scala
+++ src/main/scala/dispatcher/MoodleDispatcher.scala
@@ -89,7 +89,7 @@ class MoodleSingleResult(client: JdbcBackend#DatabaseDef, val submit: MoodleSubm
     val cval = if (r.compilation.success) "1" else "0"
     val passed = r.tests.count(_._2.success)
     client.run(
-      sqlu"""update mdl_contester_testings set finish = NOW(), compiled = ${cval}, taken = ${r.tests.size},
+      sqlu"""update mdl_contester_testings set finish = NOW(), finish_uts = UNIX_TIMESTAMP(), compiled = ${cval}, taken = ${r.tests.size},
          passed = ${passed} where ID = ${testingId}""").map(_ => ())
   }
 }
@@ -98,7 +98,7 @@ object MoodleResultReporter {
   import slick.jdbc.MySQLProfile.api._
 
   def start(client: JdbcBackend#DatabaseDef, submit: MoodleSubmit) =
-    client.run(sqlu"""Insert into mdl_contester_testings (submitid, start) values (${submit.id}, NOW())"""
+    client.run(sqlu"""Insert into mdl_contester_testings (submitid, start, start_uts) values (${submit.id}, NOW(), UNIX_TIMESTAMP())"""
       .andThen(sql"select LAST_INSERT_ID()".as[Int]).withPinnedSession)
       .map(_.head).map(new MoodleSingleResult(client, submit, _))
 }
