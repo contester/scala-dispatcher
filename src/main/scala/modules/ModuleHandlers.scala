af09bc3ad0eb357986744ff425998e650356529e
Paul Komkoff <i@stingr.net>
2020-03-22 00:38:09 +0000

    re-add visual studio.

--- src/main/scala/modules/ModuleHandlers.scala
+++ src/main/scala/modules/ModuleHandlers.scala
@@ -142,8 +142,9 @@ class Win32ModuleFactory(api: InvokerAPI) extends ModuleFactory(api) {
         (x: String) => Seq(new BCCSourceHandler(cmd, x, false), new BCCSourceHandler(cmd, x, true)))
 
   private def visualStudio(cmd: String): Future[Seq[ModuleHandler]] =
+    // api.programFiles / "Microsoft Visual Studio" / "2019"/ "Community"/ "VC" / "Auxiliary" / "Build" / "vcvars32.bat"
     // api.programFiles / "Microsoft Visual Studio*" / "Common7" / "Tools" / "vsvars32.bat"
-    add(api.programFiles / "Microsoft Visual Studio" / "2019"/ "Community"/ "VC" / "Auxiliary" / "Build" / "vcvars32.bat",
+    add(api.programFiles / "Microsoft Visual Studio*" / "Common7" / "Tools" / "vsvars32.bat",
       (x: String) => Seq(new VisualStudioSourceHandler(cmd, x), new VisualCSharpSourceHandler(cmd, x)))
 
   private def kotlin(cmd: String): Future[Seq[ModuleHandler]] =
