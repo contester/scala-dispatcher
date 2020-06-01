422c5a68ec09cbb0f48564dc731184e90d644272
Paul Komkoff <i@stingr.net>
2020-06-01 17:48:38 +0100

    Update contest/problem sync to slick schema

--- src/main/scala/dispatcher/DbScanner.scala
+++ src/main/scala/dispatcher/DbScanner.scala
@@ -5,51 +5,75 @@ import com.twitter.util.Future
 import slick.jdbc.{GetResult, JdbcBackend}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.collection.immutable
 import grizzled.slf4j.Logging
 import org.stingray.contester.polygon._
 
 import scala.concurrent.{Future => ScalaFuture}
 
-
-case class ContestRow(id: Int, name: String, polygonId: PolygonContestId, Language: String)
-case class ProblemRow(contest: Int, id: String, tests: Int, name: String)
+case class Contest(id: Int, name: String, polygonId: PolygonContestId, Language: String)
+case class Problem(contest: Int, id: String, tests: Int, name: String)
 
 class ContestNotFoundException(id: Int) extends Throwable(id.toString)
 
+object CPModel {
+  import slick.jdbc.PostgresProfile.api._
+  import com.github.tototoshi.slick.PostgresJodaSupport._
+  import com.github.nscala_time.time.Imports._
+
+  case class Contests(tag: Tag) extends Table[(Int, String, DateTime, Option[DateTime], DateTime, DateTime, String, String)](tag, "contests") {
+    def id = column[Int]("id")
+    def name = column[String]("name")
+    def startTime = column[DateTime]("start_time")
+    def freezeTime = column[Option[DateTime]]("freeze_time")
+    def endTime = column[DateTime]("end_time")
+    def exposeTime = column[DateTime]("expose_time")
+    def polygonId = column[String]("polygon_id")
+    def language = column[String]("language")
+
+    def * = (id, name, startTime, freezeTime, endTime, exposeTime, polygonId, language)
+  }
+
+  val contests = TableQuery[Contests]
+
+  case class Problems(tag: Tag) extends Table[Problem](tag, "problems") {
+    def contestID = column[Int]("contest_id")
+    def id = column[String]("id")
+    def tests = column[Int]("tests")
+    def name = column[String]("name")
+
+    def * = (contestID, id, tests, name) <> (Problem.tupled, Problem.unapply)
+  }
+
+  val problems = TableQuery[Problems]
+}
 
 object ContestTableScanner {
   case object Rescan
 
-  case class ContestMap(map: Map[ContestRow, ContestWithProblems])
+  case class ContestMap(map: Map[Contest, ContestWithProblems])
 
   case class GetContest(id: Int)
 }
 
 class ContestTableScanner(db: JdbcBackend#DatabaseDef, resolver: PolygonClient)
   extends Actor with Logging {
-  import slick.driver.MySQLDriver.api._
   import org.stingray.contester.utils.Dbutil._
   import ContestTableScanner._
 
-  implicit val getContestRow = GetResult(r =>
-    ContestRow(r.nextInt(), r.nextString(), PolygonContestId(r.nextString()), r.nextString())
-  )
+  private def getContestsFromDb = {
+    import CPModel._
+    import slick.jdbc.PostgresProfile.api._
 
-  private def getContestsFromDb =
-    db.run(sql"select id, name, polygon_id, language from contests where polygon_id != ''".as[ContestRow])
+    db.run(contests.filter(_.polygonId =!= "").map(x => (x.id, x.name, x.polygonId, x.language)).result).map(_.map(x =>
+      Contest(x._1, x._2, PolygonContestId(x._3), x._4)
+    ))
+  }
 
-  implicit val getProblemRow = GetResult(r =>
-    ProblemRow(r.nextInt(), r.nextString(), r.nextInt(), r.nextString())
-  )
-
-  private def getProblemsFromDb =
-    db.run(sql"select contest_id, id, tests, name from Problems".as[ProblemRow])
-
-  private[this] def maybeUpdateContestName(contestId: Int, rowName: String, contestName: String): Option[Future[Unit]] = {
-    import org.stingray.contester.utils.Fu._
+  private[this] def maybeUpdateContestName(contestId: Int, rowName: String, contestName: String) = {
+    import CPModel._
+    import slick.jdbc.PostgresProfile.api._
     if (rowName != contestName)
-      Some(db.run(sqlu"update contests set name = $contestName where id = $contestId").unit)
+      Some(contests.filter(_.id === contestId).map(_.name).update(contestName))
     else
       None
   }
@@ -59,18 +83,21 @@ class ContestTableScanner(db: JdbcBackend#DatabaseDef, resolver: PolygonClient)
   private def getNewContestMap =
     getContestsFromDb.flatMap { contests =>
       trace(s"received $contests")
+
       Future.collect(contests.map(x => resolver.getContest(x.polygonId).map(p => x -> p))).map(_.toMap)
     }
 
-  private def updateContest(row: ContestRow, contest: ContestWithProblems): Future[Unit] = {
+  private def updateContest(row: Contest, contest: ContestWithProblems) = {
     val nameChange = maybeUpdateContestName(row.id, row.name, contest.contest.getName(row.Language))
+    import CPModel._
+    import slick.jdbc.PostgresProfile.api._
 
-    getProblemsFromDb.flatMap { problems =>
-      val problemMap = problems.filter(_.contest == row.id).map(x => x.id.toUpperCase -> x).toMap
+    val pfixes = problems.filter(x => x.contestID === row.id).result.flatMap { probs =>
+      val problemMap = probs.filter(_.contest == row.id).map(x => x.id.toUpperCase -> x).toMap
 
-      val deletes = (problemMap.keySet -- contest.problems.keySet).map { problemId =>
-        db.run(sqlu"delete from problems where contest_id = ${row.id} and id = $problemId").unit
-      }.toSeq
+      val deletes = (problemMap.keySet -- contest.problems.keySet).toSeq.map { problemId =>
+        problems.filter(x => x.contestID === row.id && x.id === problemId).delete
+      }
 
       val updates = contest.problems.map(x => x -> problemMap.get(x._1)).collect {
         case ((problemId, polygonProblem), Some(problemRow))
@@ -78,20 +105,23 @@ class ContestTableScanner(db: JdbcBackend#DatabaseDef, resolver: PolygonClient)
           val problemTitle = polygonProblem.getTitle(row.Language)
           info(s"$problemRow | ${problemTitle} | ${polygonProblem.testCount}")
           info(s"replacing problem $problemId := $polygonProblem")
-        db.run(sqlu"""insert into problems (contest_id, id, tests, name) values (${row.id}, ${problemId},
-          ${polygonProblem.testCount}, ${problemTitle}) on conflict (contest_id, id) do update set tests = ${polygonProblem.testCount}, name = ${problemTitle}""").unit
+
+          problems.insertOrUpdate(Problem(row.id, problemId, polygonProblem.testCount, problemTitle))
+
         case (((problemId, polygonProblem), None)) =>
           val problemTitle = polygonProblem.getTitle(row.Language)
           info(s"adding problem $problemId := $polygonProblem")
-        db.run(sqlu"""insert into problems (contest_id, id, tests, name) values (${row.id}, ${problemId},
-          ${polygonProblem.testCount}, ${problemTitle}) on conflict (contest_id, id) do update set tests = ${polygonProblem.testCount}, name = ${problemTitle}""").unit
+          problems.insertOrUpdate(Problem(row.id, problemId, polygonProblem.testCount, problemTitle))
       }
-      Future.collect(deletes ++ updates ++ nameChange)
-    }.unit
+      DBIO.sequence(deletes ++ updates)
+    }
+    DBIO.seq(DBIO.sequenceOption(nameChange), pfixes)
   }
 
-  private def updateContests(m: Map[ContestRow, ContestWithProblems]) =
-    Future.collect(m.map(x => updateContest(x._1, x._2)).toSeq).unit
+  private def updateContests(m: Map[Contest, ContestWithProblems]) = {
+    import slick.jdbc.PostgresProfile.api._
+    db.run(DBIO.sequence(m.map(x => updateContest(x._1, x._2)).toSeq))
+  }
 
   import scala.concurrent.duration._
 
@@ -101,16 +131,18 @@ class ContestTableScanner(db: JdbcBackend#DatabaseDef, resolver: PolygonClient)
 
     case Rescan =>
       trace("Starting contest rescan")
-      getNewContestMap.foreach { newMap =>
+      val cm = getNewContestMap
+      cm.foreach { newMap =>
         trace(s"Contest rescan done, $newMap")
         self ! ContestMap(newMap)
         val f = updateContests(newMap)
-          f.onComplete { r =>
+          f.onComplete { _ =>
           trace("Scheduling next rescan")
           context.system.scheduler.scheduleOnce(60 seconds, self, Rescan)
         }
-        f.onFailure(e => error("rescan failed", e))
+        f.failed.foreach(e => error("rescan failed", e))
       }
+      cm.failed.foreach(e => error("rerescan failed", e))
   }
 
   context.system.scheduler.scheduleOnce(0 seconds, self, Rescan)
